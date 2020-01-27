7e495a9c322b59ac0e4d168fcb4bb0a434a3337b
Bert Bruynooghe <bert@bruynooghe-polet.com>
2020-01-27 19:56:02 +0100

    fixed remote db synching

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-
+const proxy = require('express-http-proxy')
+const remoteCouchDbHost =
+'ed4dbd8c-a8fd-4fd7-8ff5-7a9d5481f619-bluemix.cloudantnosqldb.appdomain.cloud'
+ 
+app.use('/db', proxy(remoteCouchDbHost, { https: true }))
 app.use(express.static('public/'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
